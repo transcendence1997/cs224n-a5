09103a78d133284d014258647b99338d0a93753c
朱沛丰 <zhupeifeng@zhupeifengdeMacBook-Pro.local>
2021-03-06 16:30:41 -0500

    fix 2g

--- student-new/src/attention.py
+++ student-new/src/attention.py
@@ -92,7 +92,6 @@ class SynthesizerAttention(nn.Module):
         B, T, C = x.size()
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = F.relu(self.w1(x)).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
